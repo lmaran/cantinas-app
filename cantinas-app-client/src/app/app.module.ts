b9062b223ca6a206c394f17ea7fff694a0ba75e3
Lucian Maran <lucian.maran@outlook.com>
2018-07-29 10:35:42 +0300

    install ngrx dev tools module

--- cantinas-app-client/src/app/app.module.ts
+++ cantinas-app-client/src/app/app.module.ts
@@ -11,8 +11,12 @@ import { UserDetailComponent } from './user/user-detail/user-detail.component';
 import { DishListComponent } from './dish/dish-list/dish-list.component';
 import { DishDetailComponent } from './dish/dish-detail/dish-detail.component';
 
+// NgRx
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+
 // import { reducer } from './entity/state/entity.reducer';
 import { EntityEffects } from './entity/state/entity.effects';
 
@@ -41,6 +45,11 @@ import { SharedModule } from './shared/shared.module';
         //     entity: reducer,
         // }),
         StoreModule.forRoot({}),
+        StoreDevtoolsModule.instrument({
+            name: 'Cantinas DevTools',
+            maxAge: 25,
+            logOnly: environment.production,
+        }),
         EffectsModule.forRoot([EntityEffects]),
         CoreModule.forRoot(), // forRoot => singleton
         SharedModule,
